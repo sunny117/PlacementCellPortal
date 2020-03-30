fd7d414367803057461b84476b70878af39bb645
Srikar Reddy <srikarreddymadaka@gmail.com>
2020-03-30 21:31:57 +0530

    change file name to run php script

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Placement Cell Portal</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/nav.css">
+    <script type="text/javascript" src="jquery-3.4.1/jquery-3.4.1.min.js"></script>
+</head>
+
+<body>
+    <header>    
+        <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/php/templates/"; include($IPATH."header.html"); ?>
+    </header>
+    
+    <footer>
+        <?php $IPATH = $_SERVER["DOCUMENT_ROOT"]."/php/templates/"; include($IPATH."footer.html"); ?>
+    </footer>
+</body>
+
+</html>
